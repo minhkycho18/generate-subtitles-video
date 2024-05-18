0cb3d0126d7eb935890f84ce62b728574fc9eea5
mvndang <minh.van.nhat.dang@gmail.com>
2024-05-18 22:03:14 +0700

    Refactor code

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -1,91 +1,42 @@
-from flask import Flask, request, send_file, make_response, jsonify
-from flask_restful import Resource, Api, marshal_with, fields
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, request, make_response
+from flask_restful import Resource, Api
+
+from CustomThread import CustomThread
 from main import *
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
-db = SQLAlchemy(app)
 
 api = Api(app)
 
-taskFields = {
-    'id': fields.Integer,
-    'name': fields.String,
-}
-
-fakeDatabase = {
-    1: {'name': 'Clean car'},
-    2: {'name': 'Write blog'},
-    3: {'name': 'Start stream'},
-}
 
 def generateSubtitle(inputVideo):
-    result = ""
-    video_to_audio(inputVideo, OUTPUT_AUDIO)
     createAudiosDir()
-    t1 = threading.Thread(target=slice_wav)
-    t2 = threading.Thread(target=asr)
+    video_to_audio(inputVideo, OUTPUT_AUDIO_PATH)
+    t1 = CustomThread(target=slice_wav)
+    t2 = CustomThread(target=asr, args=("",))
     t1.start()
     t1.join()  # Wait for t1 to complete before starting t2
     t2.start()
 
-    t2.join()
+    result = str(t2.join())
     removeAudiosDir()
-    with open('myfile.vtt', 'r') as file:
-        result = file.read()
     return result
 
-class Items(Resource):
-    # @marshal_with(taskFields)
+
+class Videos(Resource):
     def get(self):
         response = make_response(generateSubtitle("videoUrl"), 200)
         response.mimetype = "text/plain"
         return response
 
-    # @marshal_with(taskFields)
     def post(self):
         json_data = request.get_json(force=True)
         videoUrl = json_data['url']
         response = make_response(generateSubtitle(videoUrl), 200)
         response.mimetype = "text/plain"
         return response
-        # return jsonify(url=un)
 
-
-class Item(Resource):
-    @marshal_with(taskFields)
-    def get(self, pk):
-        return generateSubtitle()
-
-    @marshal_with(taskFields)
-    def put(self, pk):
-        data = request.json
-        task = Task.query.filter_by(id=pk).first()
-        task.name = data['name']
-        db.session.commit()
-        return task
-
-    @marshal_with(taskFields)
-    def delete(self, pk):
-        task = Task.query.filter_by(id=pk).first()
-        db.session.delete(task)
-        db.session.commit()
-        tasks = Task.query.all()
-        return tasks
-
-
-class Task(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(200), nullable=False)
-
-    def __repr__(self):
-        return self.name
-
-
-api.add_resource(Items, '/')
-
-api.add_resource(Item, '/<int:pk>')
+api.add_resource(Videos, '/')
 
 if __name__ == '__main__':
     app.run(debug=True, threaded=True)
--- flaskapp/main.py
+++ flaskapp/main.py
@@ -1,27 +1,13 @@
-import torch
-import torchaudio
-from pydub import AudioSegment
-import threading
-from pyannote.audio import Model
-from pyannote.audio.pipelines import VoiceActivityDetection
 import os
 import shutil
-from moviepy.editor import VideoFileClip
-import cloudinary
-cloudinary.config(
-    cloud_name = "dm7tnmhj4",
-    api_key = "715756646867576",
-    api_secret = "TjUu7LzW21cddLWQ45FHxm3eH3k",
-    secure = True
-)
-import cloudinary.api
-import cloudinary.uploader
 
-class Transcription:
-    def __init__(self, start_time, end_time, transcription):
-        self.startTime = start_time
-        self.endTime = end_time
-        self.transcription = transcription
+import torch
+import torchaudio
+from moviepy.editor import VideoFileClip
+from pyannote.audio import Model
+from pyannote.audio.pipelines import VoiceActivityDetection
+from pydub import AudioSegment
+
 
 class GreedyCTCDecoder(torch.nn.Module):
     def __init__(self, labels, blank=0):
@@ -35,9 +21,12 @@ class GreedyCTCDecoder(torch.nn.Module):
         indices = [i for i in indices if i != self.blank]
         return "".join([self.labels[i] for i in indices])
 
+
 FOLDER_NAME = "slice-audios"
 INPUT_VIDEO = "https://res.cloudinary.com/dm7tnmhj4/video/upload/v1716024510/my_folder/vphm3dbzxc9relgr6jmy.mp4"  # Path to input video file
-OUTPUT_AUDIO = "output_audio.wav"  # Path to output audio file
+OUTPUT_AUDIO_PATH = FOLDER_NAME + "/output_audio.wav"  # Path to output audio file
+SUBTITLE_FILE_PATH = FOLDER_NAME + "/myfile.txt"
+
 # SpeechRecognition set up
 torch.random.manual_seed(0)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -47,7 +36,7 @@ model = bundle.get_model().to(device)
 # VoiceActivityDetection set up
 # login("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", add_to_git_credential=True)
 sliced_audios = list()
-RESULT = ""
+
 
 def slice_wav():
     model = Model.from_pretrained("pyannote/segmentation-3.0", use_auth_token="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")
@@ -57,29 +46,24 @@ def slice_wav():
         "min_duration_off": 0.0
     }
     pipeline.instantiate(HYPER_PARAMETERS)
-    vad = pipeline(OUTPUT_AUDIO)
+    vad = pipeline(OUTPUT_AUDIO_PATH)
     for speech_turn, track, speaker in vad.itertracks(yield_label=True):
         # Slice audio
-        audio = AudioSegment.from_mp3(OUTPUT_AUDIO)
+        audio = AudioSegment.from_mp3(OUTPUT_AUDIO_PATH)
         start_time = speech_turn.start * 1000
         end_time = speech_turn.end * 1000
 
         sliced_audio = audio[start_time:end_time]
-        sliced_OUTPUT_AUDIOname = FOLDER_NAME + "/" + str(start_time) + "_" + str(end_time) + ".wav"
-        sliced_audio.export(sliced_OUTPUT_AUDIOname, format="wav")
+        sliced_audio_name = FOLDER_NAME + "/" + str(start_time) + "_" + str(end_time) + ".wav"
+        sliced_audio.export(sliced_audio_name, format="wav")
 
-        sliced_audios.append(sliced_OUTPUT_AUDIOname)
+        sliced_audios.append(sliced_audio_name)
 
 
-def asr():
-    open('myfile.vtt', 'w+')
+def asr(result):
     for audioSrc in sliced_audios:
         waveform, sample_rate = torchaudio.load(audioSrc)
         waveform = waveform.to(device)
-    # files = os.listdir("./slice-audios")
-    # for file in files:
-    #     waveform, sample_rate = torchaudio.load("slice-audios/" + file)
-    #     waveform = waveform.to(device)
 
         if sample_rate != bundle.sample_rate:
             waveform = torchaudio.functional.resample(waveform, sample_rate, bundle.sample_rate)
@@ -96,41 +80,10 @@ def asr():
         time_info = f"{sec2time(start_time / 1000)} --> {sec2time(end_time / 1000)}"  # subrip subtitle file time format
         text_info = transcript
         line_info = "\n" + time_info + "\n" + text_info + "\n\n"
-        print(line_info)
+        # print(line_info)
+        result += line_info
 
-        with open("myfile.vtt", "a", encoding="utf-8") as f:
-            f.writelines(line_info)
-
-def asrWithText(result):
-    open('myfile.vtt', 'w+')
-    for audioSrc in sliced_audios:
-        waveform, sample_rate = torchaudio.load(audioSrc)
-        waveform = waveform.to(device)
-        # files = os.listdir("./slice-audios")
-        # for file in files:
-        #     waveform, sample_rate = torchaudio.load("slice-audios/" + file)
-        #     waveform = waveform.to(device)
-
-        if sample_rate != bundle.sample_rate:
-            waveform = torchaudio.functional.resample(waveform, sample_rate, bundle.sample_rate)
-
-        with torch.inference_mode():
-            emission, _ = model(waveform)
-        decoder = GreedyCTCDecoder(labels=bundle.get_labels())
-        transcript = str(decoder(emission[0]))
-
-        transcript = transcript.lower().capitalize().replace("|", " ")
-        start_time, end_time = map(float, audioSrc.replace(FOLDER_NAME + "/", "").replace(".wav", "").split('_')[:2])
-
-        # Write file
-        time_info = f"{sec2time(start_time / 1000)} --> {sec2time(end_time / 1000)}"  # subrip subtitle file time format
-        text_info = transcript
-        line_info = "\n" + time_info + "\n" + text_info + "\n\n"
-        print(line_info)
-
-        with open("myfile.vtt", "a", encoding="utf-8") as f:
-            f.writelines(line_info)
-
+    return result
 
 def sec2time(sec, n_msec=3):
     if hasattr(sec, '__len__'):
@@ -157,6 +110,7 @@ def createAudiosDir():
     except OSError as e:
         print(f"Error: {folder_path} : {e.strerror}")
 
+
 def removeAudiosDir():
     folder_path = os.getcwd() + "/" + FOLDER_NAME
 
@@ -176,18 +130,3 @@ def video_to_audio(input_video, output_audio):
 
     # Write audio to file
     audio_clip.write_audiofile(output_audio)
-
-# if __name__ == "__main__":
-#     video_to_audio(INPUT_VIDEO, OUTPUT_AUDIO)
-#     createAudiosDir()
-#     t1 = threading.Thread(target=slice_wav)
-#     t2 = threading.Thread(target=asr)
-#
-#     t1.start()
-#     t1.join()  # Wait for t1 to complete before starting t2
-#     t2.start()
-#
-#     t2.join()
-#
-#     # removeAudiosDir()
-#     print("Done!")
